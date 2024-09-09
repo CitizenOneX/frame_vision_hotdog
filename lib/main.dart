8fbe4b12935e5233cc1a1024bd1ba751e5b1f1ba
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2024-09-09 14:24:30 +1000

    initial version of hotdog classifier using mlkit. added sprites for hotdog/not hotdog. temporarily picking image from filesystem not Frame

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'dart:async';
+import 'dart:io';
 import 'dart:math';
 import 'dart:typed_data';
 
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'image_data_response.dart';
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
+import 'package:image/image.dart' as img;
 import 'package:logging/logging.dart';
+
 import 'camera.dart';
+import 'image_data_response.dart';
+import 'mlkit_image_converter.dart';
 import 'simple_frame_app.dart';
 
 void main() => runApp(const MainApp());
@@ -21,28 +27,35 @@ class MainApp extends StatefulWidget {
 
 class MainAppState extends State<MainApp> with SimpleFrameAppState {
 
-  // the image and metadata to show
+  // the image to show
   Image? _image;
-  ImageMetadata? _imageMeta;
+  bool? _isHotdog;
   final Stopwatch _stopwatch = Stopwatch();
 
-  // camera settings
-  int _qualityIndex = 0;
-  final List<double> _qualityValues = [10, 25, 50, 100];
-  double _exposure = 0.0; // -2.0 <= val <= 2.0
-  int _meteringModeIndex = 0;
-  final List<String> _meteringModeValues = ['SPOT', 'CENTER_WEIGHTED', 'AVERAGE'];
-  int _autoExpGainTimes = 0; // val >= 0; number of times auto exposure and gain algorithm will be run every 100ms
-  double _shutterKp = 0.1;  // val >= 0 (we offer 0.1 .. 0.5)
-  int _shutterLimit = 6000; // 4 < val < 16383
-  double _gainKp = 1.0;     // val >= 0 (we offer 1.0 .. 5.0)
-  int _gainLimit = 248;     // 0 <= val <= 248
+  late final ImageLabeler _imageLabeler;
 
   MainAppState() {
-    Logger.root.level = Level.INFO;
+    Logger.root.level = Level.FINE;
     Logger.root.onRecord.listen((record) {
       debugPrint('${record.level.name}: ${record.time}: ${record.message}');
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // We have a 50% threshold for hotdogs so we exclude burgers etc
+    final ImageLabelerOptions options = ImageLabelerOptions(confidenceThreshold: 0.50);
+    _imageLabeler = ImageLabeler(options: options);
+  }
+
+  @override
+  void dispose() {
+    // clean up the labeler
+    _imageLabeler.close();
+
+    super.dispose();
   }
 
   @override
@@ -50,20 +63,40 @@ class MainAppState extends State<MainApp> with SimpleFrameAppState {
     currentState = ApplicationState.running;
     if (mounted) setState(() {});
 
-    // keep looping, taking photos and displaying, until user clicks cancel
+    // keep looping, waiting for photo to be sent from Frame triggered by a user tap
     while (currentState == ApplicationState.running) {
 
       try {
-        // the image metadata (camera settings) to show under the image
-        ImageMetadata meta = ImageMetadata(_qualityValues[_qualityIndex].toInt(), _autoExpGainTimes, _meteringModeValues[_meteringModeIndex], _exposure, _shutterKp, _shutterLimit, _gainKp, _gainLimit);
-
         // send the lua command to request a photo from the Frame
         _stopwatch.reset();
         _stopwatch.start();
-        await frame!.sendDataRaw(CameraSettingsMsg.pack(_qualityIndex, _autoExpGainTimes, _meteringModeIndex, _exposure, _shutterKp, _shutterLimit, _gainKp, _gainLimit));
+        // FIXME no, don't request one
+        //await frame!.sendDataRaw(CameraSettingsMsg.pack(2, 0, 0, 0.0, 0.1, 6000, 1.0, 248));
 
-        // synchronously await the image response
-        Uint8List imageData = await imageDataResponse(frame!.dataResponse, _qualityValues[_qualityIndex].toInt()).first;
+        // synchronously await the image response encoded as a jpeg
+        // FIXME no, test to see if there's an image ready or not, otherwise sleep and loop around again
+        //Uint8List imageData = await imageDataResponse(frame!.dataResponse, 50).first;
+
+        // FIXME for now let's pick a photo
+        // Open the file picker
+        FilePickerResult? result = await FilePicker.platform.pickFiles(
+          type: FileType.custom,
+          allowedExtensions: ['jpg'],
+        );
+
+        Uint8List imageData;
+        if (result != null) {
+          File file = File(result.files.single.path!);
+
+          // Read the file content and split into lines
+          imageData = await file.readAsBytes();
+        }
+        else {
+          _log.fine('User did not select a file');
+          currentState = ApplicationState.ready;
+          if (mounted) setState(() {});
+          return;
+        }
 
         // received a whole-image Uint8List with jpeg header and footer included
         _stopwatch.stop();
@@ -74,21 +107,52 @@ class MainAppState extends State<MainApp> with SimpleFrameAppState {
           // image_lib.Image? im = image_lib.decodeJpg(imageData);
           // im = image_lib.copyRotate(im, angle: 270);
 
-          // update Widget UI
-          Image im = Image.memory(imageData, gaplessPlayback: true,);
-
-          // add the size and elapsed time to the image metadata widget
-          meta.size = imageData.length;
-          meta.elapsedTimeMs = _stopwatch.elapsedMilliseconds;
-
           _log.fine('Image file size in bytes: ${imageData.length}, elapsedMs: ${_stopwatch.elapsedMilliseconds}');
 
+          // update Widget UI
           setState(() {
-            _image = im;
-            _imageMeta = meta;
+            _image = Image.memory(imageData, gaplessPlayback: true,);
           });
 
           // Perform vision processing pipeline
+          // will sometimes throw an Exception on decoding, but doesn't return null
+          _stopwatch.reset();
+          _stopwatch.start();
+          img.Image im = img.decodeJpg(imageData)!;
+          _stopwatch.stop();
+          _log.fine('Jpeg decoding took: ${_stopwatch.elapsedMilliseconds} ms');
+
+          // Android mlkit needs NV21 InputImage format
+          // iOS mlkit needs bgra8888 InputImage format
+          // In both cases orientation metadata is passed to mlkit, so no need to bake in a rotation
+          _stopwatch.reset();
+          _stopwatch.start();
+          InputImage mlkitImage = Platform.isAndroid ? rgbImageToNv21InputImage(im) : rgbImageToBgra8888InputImage(im);
+          _stopwatch.stop();
+          _log.fine('NV21/BGRA8888 conversion took: ${_stopwatch.elapsedMilliseconds} ms');
+
+          // run the image labeler
+          _stopwatch.reset();
+          _stopwatch.start();
+          final List<ImageLabel> labels = await _imageLabeler.processImage(mlkitImage);
+          _stopwatch.stop();
+          _log.fine('Image labeling took: ${_stopwatch.elapsedMilliseconds} ms');
+
+          // Check if we saw a hotdog
+          if (labels.any((label) => label.label == 'Hot dog')) {
+            _isHotdog = true;
+            var hd = labels.firstWhere((label) => label.label == 'Hot dog');
+            _log.fine('Hotdog! ${hd.confidence}');
+          }
+          else {
+            _isHotdog = false;
+            _log.fine('Not hotdog!');
+          }
+
+          // TODO for the moment just slow down the rate of photos
+          //await Future.delayed(const Duration(seconds: 5));
+          currentState = ApplicationState.ready;
+          if (mounted) setState(() {});
 
         } catch (e) {
           _log.severe('Error converting bytes to image: $e');
@@ -110,150 +174,12 @@ class MainAppState extends State<MainApp> with SimpleFrameAppState {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Frame Vision',
+      title: 'Hotdog / Not Hotdog',
       theme: ThemeData.dark(),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Frame Vision"),
+          title: const Text('Hotdog / Not Hotdog'),
           actions: [getBatteryWidget()]
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              const DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: Text('Camera Settings',
-                  style: TextStyle(color: Colors.white, fontSize: 24),
-                  ),
-              ),
-              ListTile(
-                title: const Text('Quality'),
-                subtitle: Slider(
-                  value: _qualityIndex.toDouble(),
-                  min: 0,
-                  max: _qualityValues.length - 1,
-                  divisions: _qualityValues.length - 1,
-                  label: _qualityValues[_qualityIndex].toString(),
-                  onChanged: (value) {
-                    setState(() {
-                      _qualityIndex = value.toInt();
-                    });
-                  },
-                ),
-              ),
-              ListTile(
-                title: const Text('Auto Exposure/Gain Runs'),
-                subtitle: Slider(
-                  value: _autoExpGainTimes.toDouble(),
-                  min: 0,
-                  max: 10,
-                  divisions: 10,
-                  label: _autoExpGainTimes.toInt().toString(),
-                  onChanged: (value) {
-                    setState(() {
-                      _autoExpGainTimes = value.toInt();
-                    });
-                  },
-                ),
-              ),
-              ListTile(
-                title: const Text('Metering Mode'),
-                subtitle: DropdownButton<int>(
-                  value: _meteringModeIndex,
-                  onChanged: (int? newValue) {
-                    setState(() {
-                      _meteringModeIndex = newValue!;
-                    });
-                  },
-                  items: _meteringModeValues
-                      .map<DropdownMenuItem<int>>((String value) {
-                    return DropdownMenuItem<int>(
-                      value: _meteringModeValues.indexOf(value),
-                      child: Text(value),
-                    );
-                  }).toList(),
-                ),
-              ),
-              ListTile(
-                title: const Text('Exposure'),
-                subtitle: Slider(
-                  value: _exposure,
-                  min: -2,
-                  max: 2,
-                  divisions: 8,
-                  label: _exposure.toString(),
-                  onChanged: (value) {
-                    setState(() {
-                      _exposure = value;
-                    });
-                  },
-                ),
-              ),
-              ListTile(
-                title: const Text('Shutter KP'),
-                subtitle: Slider(
-                  value: _shutterKp,
-                  min: 0.1,
-                  max: 0.5,
-                  divisions: 4,
-                  label: _shutterKp.toStringAsFixed(1),
-                  onChanged: (value) {
-                    setState(() {
-                      _shutterKp = value;
-                    });
-                  },
-                ),
-              ),
-              ListTile(
-                title: const Text('Shutter Limit'),
-                subtitle: Slider(
-                  value: _shutterLimit.toDouble(),
-                  min: 4,
-                  max: 16383,
-                  divisions: 10,
-                  label: _shutterLimit.toStringAsFixed(0),
-                  onChanged: (value) {
-                    setState(() {
-                      _shutterLimit = value.toInt();
-                    });
-                  },
-                ),
-              ),
-              ListTile(
-                title: const Text('Gain KP'),
-                subtitle: Slider(
-                  value: _gainKp,
-                  min: 1.0,
-                  max: 5.0,
-                  divisions: 4,
-                  label: _gainKp.toStringAsFixed(1),
-                  onChanged: (value) {
-                    setState(() {
-                      _gainKp = value;
-                    });
-                  },
-                ),
-              ),
-              ListTile(
-                title: const Text('Gain Limit'),
-                subtitle: Slider(
-                  value: _gainLimit.toDouble(),
-                  min: 0,
-                  max: 248,
-                  divisions: 8,
-                  label: _gainLimit.toStringAsFixed(0),
-                  onChanged: (value) {
-                    setState(() {
-                      _gainLimit = value.toInt();
-                    });
-                  },
-                ),
-              ),
-            ],
-          ),
         ),
         body: Column(
           children: [
@@ -261,53 +187,40 @@ class MainAppState extends State<MainApp> with SimpleFrameAppState {
               padding: const EdgeInsets.symmetric(horizontal: 16),
               child: Column(
                 children: [
-                  Transform(
+                  ElevatedButton(onPressed: run, child: const Text('Run!')),
+                  if (_isHotdog != null) Stack(
                     alignment: Alignment.center,
-                    // images are rotated 90 degrees clockwise from the Frame
-                    // so reverse that for display
-                    transform: Matrix4.rotationZ(-pi*0.5),
-                    child: _image,
+                    children: [
+                      Transform(
+                        alignment: Alignment.center,
+                        // images are rotated 90 degrees clockwise from the Frame
+                        // so reverse that for display
+                        transform: Matrix4.rotationZ(-pi*0.0),//FIXME Matrix4.rotationZ(-pi*0.5),
+                        child: _image,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 150,
+                        color: Colors.white54,
+                      ),
+                      if (_isHotdog != null)
+                      Row(children: [
+                        const Spacer(),
+                        Image.asset('assets/sprites/text_${_isHotdog! ? '' : 'not'}hotdog.png', width: 200),
+                        const Spacer(),
+                        Image.asset('assets/sprites/${_isHotdog! ? '' : 'not'}hotdog.png', width: 100,),
+                        const Spacer(),
+                      ],)
+                    ],
                   ),
-                  const Divider(),
-                  if (_imageMeta != null) _imageMeta!,
                 ],
               )
             ),
-            const Divider(),
           ],
         ),
         floatingActionButton: getFloatingActionButtonWidget(const Icon(Icons.camera_alt), const Icon(Icons.cancel)),
         persistentFooterButtons: getFooterButtonsWidget(),
       ),
-    );
-  }
-}
-
-class ImageMetadata extends StatelessWidget {
-  final int quality;
-  final int exposureRuns;
-  final String meteringMode;
-  final double exposure;
-  final double shutterKp;
-  final int shutterLimit;
-  final double gainKp;
-  final int gainLimit;
-
-  ImageMetadata(this.quality, this.exposureRuns, this.meteringMode, this.exposure, this.shutterKp, this.shutterLimit, this.gainKp, this.gainLimit, {super.key});
-
-  late int size;
-  late int elapsedTimeMs;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Text('Quality: $quality\nExposureRuns: $exposureRuns\nMeteringMode: $meteringMode\nExposure: $exposure'),
-        const Spacer(),
-        Text('ShutterKp: $shutterKp\nShutterLim: $shutterLimit\nGainKp: $gainKp\nGainLim: $gainLimit'),
-        const Spacer(),
-        Text('Size: ${(size/1024).toStringAsFixed(1)} kb\nTime: $elapsedTimeMs ms'),
-      ],
     );
   }
 }
